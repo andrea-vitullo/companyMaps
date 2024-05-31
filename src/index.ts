2a303763f0cbda11fd33138bdc69fda76704f7b7
Andrea Vitullo <andrea.vitullo27@gmail.com>
2024-05-31 17:32:25 +0100

    add map

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
+/// <reference types="@types/google.maps" />
+
 import { User } from './user';
 import { Company } from './Company';
 
-const user = new User();
+let map: google.maps.Map;
+async function initMap(): Promise<void> {
+    const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
 
-console.log(user);
+    map = new Map(document.getElementById("map") as HTMLElement, {
+        zoom: 1,
+        center: {
+            lat: 0,
+            lng: 0
+        }
+    });
+}
 
-const company = new Company();
-
-console.log(company);
+initMap();
